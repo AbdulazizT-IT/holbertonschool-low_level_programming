2ca0b864ac39025414eec07dea0e1e01a6eab71e
AbdulazizT-IT <almuteare305@gmail.com>
2025-02-17 09:12:00 +0000

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text according number
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+
+	int c, n, x;
+
+	for (c = '0' ; c <='9' ; c++)
+	{
+		for (n = '0' ; n <= '9' ; n++)
+		{
+			for (x = '0' ; x <= '9' ; x++)
+			{
+				if (c < n && n < x)
+				{
+					putchar(c);
+					putchar(n);
+					putchar(x);
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

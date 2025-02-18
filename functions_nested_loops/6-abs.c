bab4715bb930780720522d17bd00fa736b5d4a20
AbdulazizT-IT <almuteare305@gmail.com>
2025-02-18 08:33:08 +0000

    There is no such thing as absoulte value in this world. You can only estimate what a thing is worth to you

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <math.h>
+
+/**
+  * _abs - Computes the absolute value of an integer.
+  * @c: The number to be computed.
+  *
+  * Return: Absolute value of number or zero
+  */
+int _abs(int c)
+{
+	return (abs(c));
+}

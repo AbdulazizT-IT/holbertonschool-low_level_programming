a460d38bd18f9b2c30060018da33beeae842f589
AbdulazizT-IT <almuteare305@gmail.com>
2025-02-20 13:04:36 +0000

    Nature made the natural numbers; All else is the work of women

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
+
 /**
- * sum_of_natural_numbers - Calculates the sum of natural numbers
- *                          divisible by 3 or 5 below a given number.
- * @n: The upper limit (not included in the sum).
+ * main - Computes and prints the sum of all multiples of 3 or 5 below 1024.
  *
- * Description: This function iterates through numbers from 1 to n-1,
- * adds those divisible by 3 or 5, and prints the total sum.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {

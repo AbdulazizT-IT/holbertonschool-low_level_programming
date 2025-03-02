1505731d47c8eabdb177cc5e2cc20ef15bf768a7
AbdulazizT-IT <almuteare305@gmail.com>
2025-03-02 20:56:26 +0000

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a char in a string
+ * @s: the string
+ * @c: the char
+ *
+ * Return: the s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (s);
+}

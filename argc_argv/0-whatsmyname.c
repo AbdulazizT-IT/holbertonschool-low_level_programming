f8975f27187a164f1c4655dc70d81354815c72cd
AbdulazizT-IT <almuteare305@gmail.com>
2025-03-10 21:48:49 +0000

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program name followed by a new line
+ * @argc: Argument count (unused)
+ * @argv: Argument vector (contains program name)
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	return (0);
+}

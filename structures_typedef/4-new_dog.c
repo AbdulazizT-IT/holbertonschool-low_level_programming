bedee60745c1a9549905438411be1da3d58114a7
AbdulazizT-IT <almuteare305@gmail.com>
2025-03-17 22:01:11 +0000

    4,5

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,35 +1,77 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
+/**
+ * _strlen - returns the lenght of a string
+ * @s: pointer to s
+ *
+ * Return: 0 on success
+ *
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	if (*s != '\0')
+	{
+		while (*(s + count) != '\0')
+			count++;
+	}
+	return (count);
+}
+/**
+ * _strcpy - copies the string with \0 to the buffer
+ * @dest: pointer to dest
+ * @src: pointer to string
+ *
+ * Return: the pointer to dest
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
+/**
+ * new_dog - creates a new dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: pointer to new struct
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int len_name, len_owner;
+	struct dog *new_dog;
 
-	dog_t *d;
-
-	d = malloc(sizeof(dog_t));
-	if (d == NULL)
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
+		return (NULL);
+	len_name = _strlen(name);
+	new_dog->name = malloc(sizeof(char) * (len_name + 1));
+	if (new_dog->name == NULL)
 	{
+		free(new_dog);
 		return (NULL);
 	}
-
-	d->name = malloc(strlen(name) + 1);
-	if (d->name == NULL)
+	new_dog->name = _strcpy(new_dog->name, name);
+	new_dog->age = age;
+	len_owner = _strlen(owner);
+	new_dog->owner = malloc(sizeof(char) * (len_owner + 1));
+	if (new_dog->owner == NULL)
 	{
-		free(d);
+		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
 	}
-	strcpy(d->name, name);
-
-	d->owner = malloc(strlen(owner) + 1);
-	if (d->owner == NULL)
-	{
-		free(d->name);
-		free(d);
-		return (NULL);
-	}
-	strcpy(d->owner, owner);
-
-	d->age = age;
-
-	return (d);
+	new_dog->owner = _strcpy(new_dog->owner, owner);
+	return (new_dog);
 }

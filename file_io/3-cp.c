ca01e1b556617d091323e38b43786d614861c701
AbdulazizT-IT <almuteare305@gmail.com>
2025-04-05 14:52:50 +0000

    3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,10 +6,16 @@ int cant_read(char *);
 int cant_write(char *);
 int cant_close(int);
 
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
+ * Return: 0 on success, exits with various codes on failure.
+ */
 int main(int argc, char *argv[])
 {
 	char buffer[1024];
-	int fd_origin, fd_destiny, size = 1024, error = -1;
+	int fd_origin, fd_destiny, size, error;
 
 	if (argc != 3)
 		exit(usage_msg());
@@ -35,6 +41,7 @@ int main(int argc, char *argv[])
 			exit(cant_write(argv[2]));
 		}
 	}
+
 	if (size == -1)
 	{
 		close(fd_origin);
@@ -44,30 +51,50 @@ int main(int argc, char *argv[])
 
 	if (close(fd_origin) == -1)
 		exit(cant_close(fd_origin));
+
 	if (close(fd_destiny) == -1)
 		exit(cant_close(fd_destiny));
 
 	return (0);
 }
 
+/**
+ * usage_msg - Prints usage error message.
+ * Return: Exit code 97.
+ */
 int usage_msg(void)
 {
 	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 	return (97);
 }
 
+/**
+ * cant_read - Prints read error message.
+ * @fdin: Source file name.
+ * Return: Exit code 98.
+ */
 int cant_read(char *fdin)
 {
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fdin);
 	return (98);
 }
 
+/**
+ * cant_write - Prints write error message.
+ * @fdout: Destination file name.
+ * Return: Exit code 99.
+ */
 int cant_write(char *fdout)
 {
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fdout);
 	return (99);
 }
 
+/**
+ * cant_close - Prints close error message.
+ * @fd: File descriptor.
+ * Return: Exit code 100.
+ */
 int cant_close(int fd)
 {
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);

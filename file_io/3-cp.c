a2f0fc6b1a5cd9457d46f9e75f380d6005142f9a
AbdulazizT-IT <almuteare305@gmail.com>
2025-04-05 18:01:18 +0000

    3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,88 +1,82 @@
 #include "main.h"
 #include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 
-int usage_msg(void);
-int cant_read(char *filename);
-int cant_write(char *filename);
-int cant_close(int fd);
-
-int main(int argc, char *argv[])
+int open_source(char *file)
 {
-	int fd_from, fd_to, read_bytes, written_bytes;
+	int fd = open(file, O_RDONLY);
+	if (fd < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		exit(98);
+	}
+	return (fd);
+}
+
+int open_dest(char *file)
+{
+	int fd = open(file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (fd);
+}
+
+void copy_content(int fd_from, int fd_to, char *src, char *dest)
+{
+	ssize_t rd, wr;
 	char buffer[1024];
 
-	if (argc != 3)
-		exit(usage_msg());
-
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		exit(cant_read(argv[1]));
-
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
+	while ((rd = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
-		close(fd_from);
-		exit(cant_write(argv[2]));
-	}
-
-	if (fchmod(fd_to, 0664) == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		exit(cant_write(argv[2]));
-	}
-
-	while ((read_bytes = read(fd_from, buffer, 1024)) > 0)
-	{
-		written_bytes = write(fd_to, buffer, read_bytes);
-		if (written_bytes == -1)
+		wr = write(fd_to, buffer, rd);
+		if (wr < 0 || wr != rd)
 		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
 			close(fd_from);
 			close(fd_to);
-			exit(cant_write(argv[2]));
+			exit(99);
 		}
 	}
 
-	if (read_bytes == -1)
+	if (rd < 0)
 	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
 		close(fd_from);
 		close(fd_to);
-		exit(cant_read(argv[1]));
+		exit(98);
+	}
+}
+
+void close_fd(int fd)
+{
+	if (close(fd) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+int main(int ac, char *av[])
+{
+	int fd_from, fd_to;
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 
-	if (close(fd_from) == -1)
-		exit(cant_close(fd_from));
-
-	if (close(fd_to) == -1)
-		exit(cant_close(fd_to));
-
+	fd_from = open_source(av[1]);
+	fd_to = open_dest(av[2]);
+	copy_content(fd_from, fd_to, av[1], av[2]);
+	close_fd(fd_from);
+	close_fd(fd_to);
 	return (0);
-}
-
-int usage_msg(void)
-{
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	return (97);
-}
-
-int cant_read(char *filename)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-	return (98);
-}
-
-int cant_write(char *filename)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-	return (99);
-}
-
-int cant_close(int fd)
-{
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-	return (100);
 }

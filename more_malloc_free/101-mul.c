ac65b81a7099994fd4575508a53cd21779ca1694
AbdulazizT-IT <almuteare305@gmail.com>
2025-04-22 13:00:02 +0000

    1

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
 
 /**
- * is_digit - Checks if a string contains only digits.
- * @s: The string to check.
- *
- * Return: 1 if all characters are digits, 0 otherwise.
+ * is_digits - Checks if a string consists only of digits
+ * @s: The string to check
+ * Return: 1 if all digits, 0 otherwise
  */
-int is_digit(char *s)
+int is_digits(char *s)
 {
 while (*s)
 {
@@ -21,73 +20,72 @@ return (1);
 }
 
 /**
- * _strlen - Returns the length of a string.
- * @s: The string.
- *
- * Return: The length.
+ * print_result - Prints the multiplication result
+ * @result: Array containing the result digits
+ * @len: Length of the result array
  */
-int _strlen(char *s)
+void print_result(int *result, int len)
 {
-int len = 0;
+int i = 0;
+while (i < len && result[i] == 0)
+i++;
 
-while (s[len])
-len++;
-return (len);
+if (i == len)
+putchar('0');
+else
+{
+for (; i < len; i++)
+putchar(result[i] + '0');
+}
+putchar('\n');
 }
 
 /**
- * print_error - Prints an error message and exits.
+ * main - Multiplies two positive numbers
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: 0 on success, 98 on error
  */
-void print_error(void)
+int main(int argc, char *argv[])
+{
+char *num1, *num2;
+int len1, len2, len, i, j, n1, n2, *result, carry;
+
+if (argc != 3 || !is_digits(argv[1]) || !is_digits(argv[2]))
 {
 printf("Error\n");
 exit(98);
 }
 
-/**
- * main - Multiplies two positive numbers passed as arguments.
- * @argc: Argument count.
- * @argv: Argument vector.
- *
- * Return: 0 on success.
- */
-int main(int argc, char *argv[])
-{
-char *num1, *num2;
-int len1, len2, i, j, *result;
-int n1, n2, sum;
-
-if (argc != 3)
-print_error();
 num1 = argv[1];
 num2 = argv[2];
-if (!is_digit(num1) || !is_digit(num2))
-print_error();
-len1 = _strlen(num1);
-len2 = _strlen(num2);
-result = calloc(len1 + len2, sizeof(int));
+len1 = strlen(num1);
+len2 = strlen(num2);
+len = len1 + len2;
+
+result = calloc(len, sizeof(int));
 if (!result)
-print_error();
+{
+printf("Error\n");
+exit(98);
+}
+
 for (i = len1 - 1; i >= 0; i--)
 {
 n1 = num1[i] - '0';
+carry = 0;
+
 for (j = len2 - 1; j >= 0; j--)
 {
 n2 = num2[j] - '0';
-sum = result[i + j + 1] + n1 *n2;
-result[i + j + 1] = sum % 10;
-result[i + j] += sum / 10;
+carry += result[i + j + 1] + (n1 *n2);
+result[i + j + 1] = carry % 10;
+carry /= 10;
 }
+result[i + j + 1] += carry;
 }
-i = 0;
-while (i < len1 + len2 && result[i] == 0)
-i++;
-if (i == len1 + len2)
-putchar('0');
-else
-for (; i < len1 + len2; i++)
-putchar(result[i] + '0');
-putchar('\n');
+
+print_result(result, len);
 free(result);
 return (0);
 }
